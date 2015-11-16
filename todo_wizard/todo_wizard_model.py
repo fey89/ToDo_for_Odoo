64d453c062a02b1b06bd7e7a561a0ae6758aceab
Fey89 <feyza.kozan04@gmail.com>
2015-11-16 12:59:01 +0100

    mass method in wizard model

--- todo_wizard/todo_wizard_model.py
+++ todo_wizard/todo_wizard_model.py
@@ -11,3 +11,16 @@ class TodoWizard(models.TransientModel):
 	new_deadline = fields.Date('Deadline to Set')
 	new_user_id = fields.Many2one(
 	'res.users',string='Responsible to Set')
+
+	@api.multi
+	def do_mass_update(self):
+		self.ensure_one()
+		if not (self.new_deadline or self.new_user_id):
+			raise exceptions.ValidationError('No data to update!')
+		# else:
+			_logger.debug('Mass update on Todo Tasks %s', self.task_ids.ids)
+		if self.new_deadline:
+			self.task_ids.write({'date_deadline': self.new_deadline})
+		if self.new_user_id:
+			self.task_ids.write({'user_id': self.new_user_id.id})
+		return True
