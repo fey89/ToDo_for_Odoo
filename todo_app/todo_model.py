e894d56023cf161697f473e81b19080788e0e245
Fey89 <feyza.kozan04@gmail.com>
2015-11-16 17:55:08 +0100

    add create method in todo.task model

--- todo_app/todo_model.py
+++ todo_app/todo_model.py
@@ -17,4 +17,13 @@ class TodoTask(models.Model):
 		done_recs.write({'active':False})
 		return True
 
+	@api.model
+	def create(self, vals):
+		# Code before create
+		# Can use the `vals` dict
+		new_record = super(TodoTask, self).create(vals)
+		# Code after create
+		# Can use the `new` record created
+		return new_record
+
 	
